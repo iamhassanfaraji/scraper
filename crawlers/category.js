33526afb78f6d13e0b67767ba679c4ad81e9a2e7
hassanfaraji <iamhassanfaraji@gmail.com>
2023-05-11 06:04:45 -0600

    some changes

--- crawlers/category.js
+++
@@ -1,9 +0,0 @@
-async function crawler(page, readData){
-    const result = await readData(
-        '.base-layout-desktop-header-navigation_BaseLayoutDesktopHeaderNavigation__navGroup__bGWtA',
-        ".BaseLayoutDesktopHeaderNavigationMainMegaMenu_BaseLayoutDesktopHeaderNavigationMainMegaMenu__mainCategoriesSection__QDkXq a p"
-    )
-    console.log(result)
-}
-
-module.exports = crawler
