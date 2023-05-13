74572c477bfeb6ad9e0e573b208810922f62da53
hassanfaraji <iamhassanfaraji@gmail.com>
2023-05-13 06:50:56 -0600

    structed of scraped changed, should update

--- importData/categories.js
+++ importData/categories.js
@@ -4,7 +4,6 @@ const fs = require("fs")
 
 
 fs.readFile(join(__dirname, "/../result/category.json"), { encoding: "utf-8" }, async (err, data) => {
-
     const categories = JSON.parse(data)
 
     if (err) {
@@ -16,27 +15,17 @@ fs.readFile(join(__dirname, "/../result/category.json"), { encoding: "utf-8" }, 
 
         try {
 
-            for (let firstLevel in categories) {
+            for (let category of categories) {
 
-                const nameCategoryFirstLevel = firstLevel
+                const nameCategoryFirstLevel = category.firstLevel.name
                 await queryAsync("INSERT INTO product_categories(name) VALUES(?)", [nameCategoryFirstLevel])
-                const firstLevelId = await queryAsync("SELECT id FROM product_categories WHERE name=?", [nameCategoryFirstLevel])
+                const firstLevelId = (await queryAsync("SELECT id FROM product_categories WHERE name=?", [nameCategoryFirstLevel]))[0].id
 
-                for (let midLevel of categories[firstLevel]) {
-
-                    const nameCategoryMidLevel = midLevel.level2.name
-                    await queryAsync("INSERT INTO product_categories(name, parent_id)  VALUES(?, ?)", [nameCategoryMidLevel, firstLevelId[0].id])
-                    const midLevelId = await queryAsync("SELECT id FROM product_categories WHERE name=?", [nameCategoryMidLevel])
-
-                    if (midLevel.level3.length > 0) {
-
-                        for (let lastLevel of midLevel.level3) {
-                            const nameCategoryLastLevel = lastLevel.value
-                            await queryAsync("INSERT INTO product_categories(name, parent_id)  VALUES(?, ?)", [nameCategoryLastLevel, midLevelId[0].id])
-                        }
-
+                if (category.lastLevel.length > 0) {
+                    for (let lastLevel of category.lastLevel) {
+                        const nameCategoryLastLevel = lastLevel.value
+                        await queryAsync("INSERT INTO product_categories(name, parent_id)  VALUES(?, ?)", [nameCategoryLastLevel, firstLevelId])
                     }
-
                 }
 
                 console.log(`category ${nameCategoryFirstLevel} added succussFully`)
